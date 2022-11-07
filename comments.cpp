def0908f23e63c7f7dfa27550faa971e2c37bff3
MD Hasan Ahmad <82166280+hasanahmad676@users.noreply.github.com>
2022-11-07 16:08:57 +0600

    Add files via upload

---
+++ comments.cpp
@@ -0,0 +1,40 @@
+// for comment out something we use (//) for one line and for multiple line (/* */)
+// code is not efficint 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+ 
+cout<<"hello world"<<endl;
+cout<<"this world is mine "<<endl;
+return 0;
+
+
+}
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+cout<<"hello world"<<endl;
+cout<<"this world is mine "<<endl;
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
